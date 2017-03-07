c6fc05c45d2fd8ba0e0526e20e72cbb159b22adc
ludvikkemp <ludvik11@ru.is>
2017-03-07 15:38:27 +0000

    merge conflict fixes

--- sales/src/app/app.component.ts
+++ sales/src/app/app.component.ts
@@ -13,38 +13,6 @@ export class AppComponent {
 
   constructor() {}
 
-<<<<<<< HEAD
-  addSeller() {
-    console.log('addSeller() clicked');
-    const modalInstance = this.modalService.open(SellerDialogComponent);
-    modalInstance.componentInstance.sellerName = 'Lúlli';
-    modalInstance.componentInstance.category = 'Flokkur';
-    modalInstance.componentInstance.imgPath = 'Mynd';
-    modalInstance.result.then(obj => {
-      console.log('When pressed OK');
-      console.log('Dialog object :', obj);
-    }).catch(err => {
-      console.log('When pressed Cancel');
-      console.log(err);
-    });
-  }
-
-  editSeller(s: Seller) {
-    console.log(s);
-    const modelInstance = this.modalService.open(SellerDialogComponent);
-    modelInstance.componentInstance.id = s.id;
-    modelInstance.componentInstance.sellerName = s.name;
-    modelInstance.componentInstance.category = s.category;
-    modelInstance.componentInstance.imgPath = s.imagePath;
-    modelInstance.result.then(obj => {
-      console.log('When pressed OK');
-      console.log(obj);
-    }).catch(err => {
-      console.log('When presses Cancel');
-      console.log(err);
-    });
-  }*/
-=======
   ngOnInit() {}
->>>>>>> 72c8491900b99363bd40ae43cb6c1696cdeac161
+
 }
