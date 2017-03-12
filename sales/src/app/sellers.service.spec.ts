88cb931f89a83090568cc0a4fb245f1e343fccba
kari14@ru.is <karieiriks>
2017-03-12 11:56:43 +0000

    Toastr and stuff

--- sales/src/app/sellers.service.spec.ts
+++ sales/src/app/sellers.service.spec.ts
@@ -2,15 +2,63 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { SellersService } from './sellers.service';
+import { FormsModule } from '@angular/forms';
+import { Http, Headers } from '@angular/http';
+import { Observable} from 'rxjs/Observable';
+import { Seller, Product } from './sellers.service';
 
 describe('SellersService', () => {
+  
+  class MockHttp {
+    sellers = [{
+      id: 1,
+      name: 'Kári',
+      category: 'Matur',
+    }, {
+      id: 2,
+      name: 'Lúlli',
+      category: 'Föt',
+    }];
+
+    getSellers(): Observable<Seller[]> {
+      return Observable.of(this.sellers);
+    }
+
+    post() {
+
+    }
+
+    put() {
+
+    }
+  }
+
+  let mockHttp = new MockHttp();
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [SellersService]
+       imports: [ FormsModule ],
+      providers: [{
+        provide: Http,
+        useValue: mockHttp
+      }]
     });
   });
 
   /*xit('should ...', inject([SellersService], (service: SellersService) => {
     expect(service).toBeTruthy();
   }));*/
+
+  it('should get all sellers from Api', () => {
+    let sellers = [{
+      id: 1,
+      name: 'Kári',
+      category: 'Matur',
+    }, {
+      id: 2,
+      name: 'Lúlli',
+      category: 'Föt',
+    }];
+    expect(mockHttp.getSellers()).toEqual(sellers);
+  })
 });
